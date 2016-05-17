f7ae05c863faaec5d2eab169bb585e30378ee15d
Thomas Brüggemann <mail@thomasbrueggemann.com>
2016-05-17 13:34:29 +0200

    small improvements

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function() {
 	var p = navigator.platform;
 
 	// mac
-	if (p.indexOf("Mac") !== -1) {
+	/*if (p.indexOf("Mac") !== -1) {
 		$("#dlWin,#dlLin").hide();
 		$("#dlMac").show();
 	}
@@ -17,5 +17,11 @@ $(function() {
 	else if (p.indexOf("Linux") !== -1) {
 		$("#dlMac,#dlWin").hide();
 		$("#dlLin").show();
+	}
+
+	// Android or iDevice
+	else */
+	if (p.indexOf("Android") !== -1 || p.indexOf("iPad") !== -1 || p.indexOf("iPod") !== -1 || p.indexOf("iPhone") !== -1) {
+		$("#desktopRow").hide();
 	}
 });
